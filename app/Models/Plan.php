80b574bd5c90a97c397f65259e2ca6b6943a32ea
franciscoleal <francisco.lleal@gmail.com>
2020-04-14 00:58:37 -0300

    criado regras para o filtro e o relacionamento entre DetailPlan e Plan

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Plan extends Model
 {
     protected $fillable = ['name', 'url', 'price', 'description'];
+
+    public function details(){
+        return $this->hasMany(DetailPlan::class);
+    }
+
+    public function search($filter = null){
+        $results = $this->where('name', 'LIKE', "%{$filter}%")
+                        ->orwhere('description', 'LIKE', "%{$filter}%")
+                        ->paginate(1);
+        return $results;
+    }
 }
