df25db0278462a0d19a96bba61dd4a8f5cda5a56
franciscoleal <francisco.lleal@gmail.com>
2020-04-14 00:55:48 -0300

    criado view para form de editar

---
+++ resources/views/admin/pages/plans/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('adminlte::page')
+
+@section('title', "Editar o Plano {$plan->name}")
+
+@section('content_header')
+<h1>Editar o Plano {{ $plan->name }}</h1>
+@stop
+
+@section('content')
+
+<div class="card">
+
+    <div class="card-header">
+        #Cadastro
+    </div>
+    <div class="card-body">
+    <form action="{{ route('plans.update', $plan->url) }}" class="form" method="POST">
+            @csrf
+            @method('PUT')
+
+            @include('admin.pages.plans._partials.form')
+       </form>
+    </div>
+    <div class="card-footer"></div>
+</div>
+
+@stop
