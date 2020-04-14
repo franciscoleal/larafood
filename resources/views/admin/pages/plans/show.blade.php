4ada210bed9e95f7f15572f735bc06b498c5bb0e
franciscoleal <francisco.lleal@gmail.com>
2020-04-14 00:56:29 -0300

    criado view dos detalhes dos planos

---
+++ resources/views/admin/pages/plans/show.blade.php
@@ -0,0 +1,42 @@
+@extends('adminlte::page')
+
+@section('title', "Detalhes do Plano {$plan->name}")
+
+@section('content_header')
+<h1>Detalhes do Plano</h1>
+@stop
+
+@section('content')
+
+<div class="card">
+
+    <div class="card-header">
+        <b>{{ $plan->name }}</b>
+    </div>
+    <div class="card-body">
+        <ul>
+            <li>
+                <strong>Nome: </strong> {{ $plan->name }}
+            </li>
+            <li>
+                <strong>Url: </strong> {{ $plan->url }}
+            </li>
+            <li>
+                <strong>Preço: </strong> R$ {{ number_format($plan->price, 2, ',', '.') }}
+            </li>
+            <li>
+                <strong>Descrição: </strong> {{ $plan->description }}
+            </li>
+        </ul>
+
+        <form action="{{ route('plans.destroy', $plan->url) }}" method="POST">
+            @csrf
+            @method('DELETE')
+        <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i> Deletar o Plano - {{ $plan->name }}</button>
+        </form>
+
+    </div>
+    <div class="card-footer"></div>
+</div>
+
+@stop
