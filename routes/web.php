2daad499178a5146d7a777b6e4ef83b8e09469d9
franciscoleal <francisco.lleal@gmail.com>
2020-04-14 00:57:03 -0300

    organizado as routas por prefixo, group e namespace

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,29 @@
 <?php
 
-Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
+Route::prefix('admin')->namespace('Admin')->group(function(){
+
+    // Routes Detaisl Plans
+
+    Route::get('plans/{url}/details', 'DetailPlanController@index')->name('details.plan.index');
+
+    // Routes Plans
+
+    Route::get('plans/create', 'PlanController@create')->name('plans.create');
+    Route::put('plans/{url}', 'PlanController@update')->name('plans.update');
+    Route::get('plans/{url}/edit', 'PlanController@edit')->name('plans.edit');
+    Route::any('plans/search', 'PlanController@search')->name('plans.search');
+    Route::delete('plans/{url}', 'PlanController@destroy')->name('plans.destroy');
+    Route::get('plans/{url}', 'PlanController@show')->name('plans.show');
+    Route::post('plans', 'PlanController@store')->name('plans.store');
+    Route::get('plans', 'PlanController@index')->name('plans.index');
+
+    // Home Dashboard
+
+    Route::get('/', 'PlanController@index')->name('admin.index');
+
+});
+
+
 
 Route::get('/', function () {
     return view('welcome');
