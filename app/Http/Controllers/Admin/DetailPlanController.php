9fd3dfb9f5e020715aef281771ddd6232c56e1ab
franciscoleal <francisco.lleal@gmail.com>
2020-06-23 21:13:34 -0300

    criado o controller DetailPlanController e a função index

--- app/Http/Controllers/Admin/DetailPlanController.php
+++ app/Http/Controllers/Admin/DetailPlanController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\DetailPlan;
+use App\Models\Plan;
 use Illuminate\Http\Request;
 
 class DetailPlanController extends Controller
 {
-    //
+    protected $repository, $plan;
+
+    public function _construct(DetailPlan $detailPlan, Plan $plan){
+        $this->repository = $detailPlan;
+        $this->plan = $plan;
+    }
+
+    public function index($urlPlan){
+
+        // dd($urlPlan);
+
+        if (!$plan = $this->plan->where('url', $urlPlan)->first()) {
+            return redirect()->back();
+        }
+
+        // $details = $plan->details();
+        $details = $plan->details()->paginate();
+
+        return view('admin.pages.plans.details.index', [
+            'plan' => $plan,
+            'details' => $details,
+        ]);
+    }
+
 }
